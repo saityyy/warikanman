253774f825b9e0500ba02d3eb2f2f9b5416910fb
saityyy <sec200091@gmail.com>
2021-05-27 19:07:21 +0000

    a

--- main.py
+++ main.py
@@ -44,13 +44,12 @@ def callback():
 #def以下の関数を実行します。
 #reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
 #第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
- 
+
+List=[] 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     mes=event.message.text
-    print(event.message)
-    print(event.message.text)
-    print(mes)
+    list.append(mes)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
