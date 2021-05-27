e88e406057b4b3703fce1936933059808ccea875
saityyy <sec200091@gmail.com>
2021-05-27 19:09:26 +0000

    a

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ List=[]
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     mes=event.message.text
-    list.append(mes)
+    List.append(mes)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
