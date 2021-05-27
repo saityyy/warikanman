c72b643ce8985529b76709ecbbc1f91caaec03a7
saityyy <sec200091@gmail.com>
2021-05-27 18:58:41 +0000

    a

--- main.py
+++ main.py
@@ -47,7 +47,9 @@ def callback():
  
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print(event.message)
+    mes=event.message.text
+    print(mes)
+    print(mes.decode("unicode-escape"))
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
