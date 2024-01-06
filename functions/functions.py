f035944b8b402c82fb37fccfd1d9e3edb367d2e5
saityyy <sec200091@gmail.com>
2024-01-06 04:42:53 +0000

    リファクタリング

--- functions/functions.py
+++ functions/functions.py
@@ -1,4 +1,27 @@
-def create_projects(conn, date_time, project_id, participant_number):
+import datetime
+from typing import Any, TypedDict, Literal
+
+# alias typing
+
+
+class MessageResult(TypedDict):
+    type: Literal["help",
+                  "project",
+                  "log",
+                  "check",
+                  "delete",
+                  "pay",
+                  "pass"]
+    args: tuple
+    isValid: bool
+    error_message: str
+
+
+def create_projects(conn,
+                    date_time: datetime.datetime,
+                    project_id: str,
+                    participant_number: int
+                    ) -> str:
     cur = conn.cursor(dictionary=True)
     # すでにプロジェクトが存在する場合は削除
     cur.execute("SELECT * FROM projects WHERE project_id=%s;", (project_id,))
@@ -7,72 +30,81 @@ def create_projects(conn, date_time, project_id, participant_number):
     cur.execute("INSERT INTO projects (project_id,datetime,participant_number) VALUES(%s,%s,%s);",
                 (project_id, date_time, participant_number))
     conn.commit()
-    res = "参加人数{}人の割り勘プロジェクトを作成しました".format(participant_number)
-    return res
+    reply_message = "参加人数{}人の割り勘プロジェクトを作成しました".format(participant_number)
+    return reply_message
 
 
-def add_payment(conn, project_id, user_id: str, name, date_time, amount, message):
+def add_payment(conn,
+                project_id: str,
+                user_id: str,
+                name: str,
+                date_time: datetime.datetime,
+                amount: int,
+                message: str
+                ) -> str:
     cur = conn.cursor(dictionary=True)
     cur.execute("SELECT * FROM projects WHERE project_id=%s;", (project_id,))
-    res = cur.fetchall()
-    if len(res) == 0:
+    project = cur.fetchall()
+    if len(project) == 0:
         return "プロジェクトが存在しません"
-    participant_number = res[0]["participant_number"]
+    participant_number: int = project[0]["participant_number"]
     cur.execute(
         "SELECT DISTINCT user_id FROM payments WHERE project_id=%s;", (project_id,))
-    res = cur.fetchall()
-    if len(res) == participant_number and (user_id not in [row["user_id"] for row in res]):
+    # すでに支払い記録があるユーザーのuser_idを取得
+    pay_user_ids = [row["user_id"] for row in cur.fetchall()]
+    if len(pay_user_ids) == participant_number and (user_id not in pay_user_ids):
         return "設定した参加人数を超えています"
     cur.execute("SELECT name FROM users WHERE user_id=%s;", (user_id,))
-    result = cur.fetchall()
+    user = cur.fetchall()
     # ユーザーが存在しない場合は追加
-    if len(result) == 0:
+    if len(user) == 0:
         cur.execute(
             "INSERT INTO users (user_id,name) VALUES(%s,%s);", (user_id, name))
     # ユーザー名が変更されている場合は更新
-    elif result[0]["name"] != name:
+    elif user[0]["name"] != name:
         cur.execute("UPDATE users SET name=%s WHERE user_id=%s;",
                     (name, user_id))
     cur.execute("INSERT INTO payments (project_id,user_id,datetime,amount,message) VALUES(%s,%s,%s,%s,%s);",
                 (project_id, user_id, date_time, amount, message))
     conn.commit()
-    res = "記録しました\n"
-    res += "{} : {}円\n".format(name, amount)
+    reply_message = "記録しました\n"
+    reply_message += "{} : {}円\n".format(name, amount)
     if message != "":
-        res += "メッセージ : {}".format(message)
-    return res
+        reply_message += "メッセージ : {}".format(message)
+    return reply_message
 
 
-def check_payments_log(conn, project_id):
+def check_payments_log(conn, project_id: str) -> str:
     cur = conn.cursor(dictionary=True)
     cur.execute(
         "SELECT participant_number FROM projects WHERE project_id=%s;", (project_id,))
     project = cur.fetchall()
     if len(project) == 0:
         return "プロジェクトが存在しません"
-    participant_number = project[0]["participant_number"]
+    participant_number: int = project[0]["participant_number"]
+    # あるプロジェクトでの支払い記録をdatetimeでソートして取得
     query = """
-        SELECT id,datetime,(SELECT name from users WHERE user_id=p.user_id) user_name,
-        amount,message FROM payments p WHERE project_id=
-        (SELECT project_id FROM projects WHERE project_id=%s)
+        SELECT id,datetime,(SELECT name from users WHERE user_id=p.user_id)
+        user_name,amount,message FROM payments p
+        WHERE project_id=(SELECT project_id FROM projects WHERE project_id=%s)
         ORDER BY datetime ASC;
     """
     cur.execute(query, (project_id,))
-    res = "割り勘参加人数：{}人\n\n".format(participant_number)
+    reply_message = "割り勘参加人数 : {}人\n\n".format(participant_number)
     result = cur.fetchall()
     for idx, row in enumerate(result, start=1):
         datetime_str = row["datetime"].strftime("%-m/%-d %-H:%M")
-        res += "{}) {}\n".format(idx, datetime_str)
-        res += "{}：{}円\n".format(row["user_name"], row["amount"])
+        reply_message += "{}) {}\n".format(idx, datetime_str)
+        reply_message += "{} : {}円\n".format(row["user_name"], row["amount"])
         if row["message"] != "":
-            res += "メッセージ：{}\n".format(row["message"])
-        res += "\n"
-    res += "合計 : {}円".format(sum([row["amount"] for row in result]))
+            reply_message += "メッセージ : {}\n".format(row["message"])
+        reply_message += "\n"
+    reply_message += "合計 : {}円".format(sum([row["amount"] for row in result]))
     conn.commit()
-    return res
+    return reply_message
 
 
-def warikan(conn, project_id):
+def warikan(conn, project_id: str) -> str:
     cur = conn.cursor(dictionary=True)
     # paymentsテーブルからプロジェクトIDが一致するものを取得
     query = """
@@ -84,41 +116,41 @@ def warikan(conn, project_id):
     # project's participants number
     cur.execute(
         "SELECT participant_number FROM projects WHERE project_id=%s;", (project_id,))
-    project = cur.fetchall()
+    project: list[dict[str, Any]] = cur.fetchall()
     if len(project) == 0:
         return "プロジェクトが存在しません"
-    participant_number = project[0]["participant_number"]
-    user2amount = {}
+    participant_number: int = project[0]["participant_number"]
+    username2amount: dict[str, int] = {}
     # 合計金額を計算
-    total_amount = 0
+    total_amount: int = 0
     for row in result:
         total_amount += row["amount"]
-        if row["user_name"] not in user2amount.keys():
-            user2amount[row["user_name"]] = 0
-        user2amount[row["user_name"]] += row["amount"]
+        if row["user_name"] not in username2amount.keys():
+            username2amount[row["user_name"]] = 0
+        username2amount[row["user_name"]] += row["amount"]
     fraction = total_amount % participant_number
     amount_per_user = int(total_amount/participant_number)
     if total_amount == 0:
         return "支払い記録がまだひとつもありません"
-    res = "合計金額 : {}円\n".format(total_amount)
-    res += "参加人数 : {}人\n".format(participant_number)
-    res += "一人あたりの金額 : {}円\n\n".format(amount_per_user)
-    res += "端数 : {}円\n\n".format(fraction)
-    for name, amount in user2amount.items():
-        res += "{} : {}円".format(name, amount)
+    reply_message = "合計金額 : {}円\n".format(total_amount)
+    reply_message += "参加人数 : {}人\n".format(participant_number)
+    reply_message += "一人あたりの金額 : {}円\n\n".format(amount_per_user)
+    reply_message += "端数 : {}円\n\n".format(fraction)
+    for name, amount in username2amount.items():
+        reply_message += "{} : {}円".format(name, amount)
         if amount > amount_per_user:
-            res += "（{}円もらう）\n".format(amount-amount_per_user)
+            reply_message += "（{}円もらう）\n".format(amount-amount_per_user)
         else:
-            res += "（{}円はらう）\n".format(amount_per_user-amount)
-    other_num = participant_number-len(user2amount)
+            reply_message += "（{}円はらう）\n".format(amount_per_user-amount)
+    other_num = participant_number-len(username2amount)
     if other_num > 0:
-        res += "その他の参加者：0円（{}円はらう）\n".format(amount_per_user)
-    res = res[:-1]
-    return res
+        reply_message += "その他の参加者：0円（{}円はらう）\n".format(amount_per_user)
+    reply_message = reply_message[:-1]
+    return reply_message
 
 
-def delete_payment(conn, project_id, delete_number):
-    # project_idが一致するものをdatetimeでソートして、古い方からindex番目のものを削除
+# あるプロジェクトでの支払い記録をdatetimeでソートして、古い方からindex番目のものを削除
+def delete_payment(conn, project_id: str, delete_number: int) -> str:
     query = """
         SELECT id FROM payments WHERE project_id=
         (SELECT project_id FROM projects WHERE project_id=%s)
@@ -137,67 +169,65 @@ def delete_payment(conn, project_id, delete_number):
     return "{}番の記録を削除しました".format(delete_number)
 
 
-def extract_message(message):
+def extract_message(message: str) -> MessageResult:
     messages = message.split()
-    res = {
-        "type": "",
+    message_result: MessageResult = {
+        "type": "pass",
         "args": (),
         "isValid": True,
         "error_message": ""
     }
     if messages[0] == "!help":
-        res["type"] = "help"
-        return res
+        message_result["type"] = "help"
+        return message_result
     if messages[0] == "!check":
-        res["type"] = "check"
-        return res
+        message_result["type"] = "check"
+        return message_result
     if messages[0] == "!log":
-        res["type"] = "log"
-        return res
+        message_result["type"] = "log"
+        return message_result
     if messages[0] == "!project":
-        res["type"] = "project"
+        message_result["type"] = "project"
         if len(message.split()) == 1:
-            res["isValid"] = False
-            res["error_message"] = "プロジェクトの参加人数を半角数字で入力してください"
-            return res
+            message_result["isValid"] = False
+            message_result["error_message"] = "プロジェクトの参加人数を半角数字で入力してください"
+            return message_result
         participant_number = message.split()[1]
         if not (participant_number.isdigit() and int(participant_number) >= 2):
-            res["isValid"] = False
-            res["error_message"] = "参加人数は2以上の半角数字で入力してください"
-            return res
-        res["args"] = (int(participant_number),)
-        return res
+            message_result["isValid"] = False
+            message_result["error_message"] = "参加人数は2以上の半角数字で入力してください"
+            return message_result
+        message_result["args"] = (int(participant_number),)
+        return message_result
     if messages[0] == "!delete":
-        res["type"] = "delete"
+        message_result["type"] = "delete"
         if len(message.split()) == 1:
-            res["isValid"] = False
-            res["error_message"] = "削除する記録の通し番号を半角数字で入力してください"
-            return res
+            message_result["isValid"] = False
+            message_result["error_message"] = "削除する記録の通し番号を半角数字で入力してください"
+            return message_result
         index = message.split()[1]
         if not (index.isdigit() and int(index) >= 1):
-            res["isValid"] = False
-            res["error_message"] = "通し番号の指定は1以上の半角数字で入力してください"
-            return res
-        res["args"] = (int(index),)
-        return res
+            message_result["isValid"] = False
+            message_result["error_message"] = "通し番号の指定は1以上の半角数字で入力してください"
+            return message_result
+        message_result["args"] = (int(index),)
+        return message_result
     elif messages[0] == "!pay":
-        res["type"] = "pay"
+        message_result["type"] = "pay"
         if len(message.split()) == 1:
-            res["isValid"] = False
-            res["error_message"] = "はらった金額を半角数字で入力してください"
-            return res
+            message_result["isValid"] = False
+            message_result["error_message"] = "はらった金額を半角数字で入力してください"
+            return message_result
         amount = messages[1]
         pay_message = " ".join(messages[2:])
         if not (amount.isdigit() and int(amount) > 0):
-            res["isValid"] = False
-            res["error_message"] = "はらった金額は1以上の半角数字で入力してください"
-            return res
+            message_result["isValid"] = False
+            message_result["error_message"] = "はらった金額は1以上の半角数字で入力してください"
+            return message_result
         if len(pay_message) > 150:
-            res["isValid"] = False
-            res["error_message"] = "メッセージが長すぎます"
-            return res
-        res["args"] = (int(amount), pay_message)
-        return res
-    else:
-        res["type"] = "pass"
-    return res
+            message_result["isValid"] = False
+            message_result["error_message"] = "メッセージが長すぎます"
+            return message_result
+        message_result["args"] = (int(amount), pay_message)
+        return message_result
+    return message_result
--- main.py
+++ main.py
@@ -44,70 +44,86 @@ def callback():
 
 
 TEMP_TIMESTAMP = "2020-01-01 00:00:00"
-project = {}
+
+
+def get_send_datetime(event: MessageEvent) -> datetime.datetime:
+    if event.timestamp is None:
+        # 現在の時刻を返す
+        return datetime.datetime.now(tz=ZoneInfo("Asia/Tokyo"))
+    return datetime.datetime.fromtimestamp(int(event.timestamp/1000),
+                                           tz=ZoneInfo("Asia/Tokyo"))
+
+
+def get_project_id(event: MessageEvent) -> str:
+    if event.source.type == "group":
+        return str(event.source.group_id)
+    else:
+        return str(event.source.user_id)
+
+
+def get_user_name(user_id: str) -> str:
+    user_name = line_bot_api.get_profile(user_id).display_name
+    if user_name is None:
+        return "名無しさん_"+str(user_id)
+    else:
+        return user_name
+
+
+def print_log(time: datetime.datetime, project_id: str, command: str) -> None:
+    print("[{} , {} , {}]".format(time, project_id, command))
+
+
+def send_reply(_token, _textmessage):
+    line_bot_api.reply_message(
+        _token,
+        TextSendMessage(text=_textmessage)
+    )
 
 
 @ handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-
+def handle_message(event: MessageEvent):
     if not conn.is_connected():
         conn.ping(reconnect=True, attempts=3, delay=3)
-    mes = str(event.message.text)
-    date_time = datetime.datetime.fromtimestamp(int(event.timestamp/1000),
-                                                tz=ZoneInfo("Asia/Tokyo"))
-    print("送信メッセージ : {}".format(mes))
-    source_type = event.source.type
-    user_id = event.source.user_id
-    if source_type == "group":
-        project_id = str(event.source.group_id)
-    elif source_type == "room":
-        project_id = str(event.source.room_id)
-    else:
-        project_id = str(user_id)
-    user = line_bot_api.get_profile(user_id).display_name
-    message_result = functions.extract_message(mes)
-    print(message_result)
+    date_time = get_send_datetime(event)
+    project_id = get_project_id(event)
+    user_id: str = event.source.user_id
+    user_name = get_user_name(user_id)
+    message_result = functions.extract_message(str(event.message.text))
+    print_log(date_time, project_id, message_result["type"])
     if message_result["type"] == "pass":
         pass
     elif not message_result["isValid"]:
-        send(event.reply_token, message_result["error_message"])
+        send_reply(event.reply_token, message_result["error_message"])
     # !project <参加人数>
     elif message_result["type"] == "project":
         participant_number, = message_result["args"]
         res = functions.create_projects(
             conn, date_time, project_id, participant_number)
-        send(event.reply_token, res)
+        send_reply(event.reply_token, res)
     # !log
     elif message_result["type"] == "log":
         res = functions.check_payments_log(conn, project_id)
-        send(event.reply_token, res)
+        send_reply(event.reply_token, res)
     # !pay <金額> <メッセージ>
     elif message_result["type"] == "pay":
         amount, message = message_result["args"]
         res = functions.add_payment(
-            conn, project_id, user_id, user, date_time, amount, message)
-        send(event.reply_token, res)
+            conn, project_id, user_id, user_name, date_time, amount, message)
+        send_reply(event.reply_token, res)
     # !check
     elif message_result["type"] == "check":
         res = functions.warikan(conn, project_id)
-        send(event.reply_token, res)
+        send_reply(event.reply_token, res)
     # !delete <削除したい記録の通し番号>
     elif message_result["type"] == "delete":
         del_index, = message_result["args"]
         res = functions.delete_payment(conn, project_id, int(del_index))
-        send(event.reply_token, res)
+        send_reply(event.reply_token, res)
     # !help
     elif message_result["type"] == "help":
         f = open("./help.txt")
-        send(event.reply_token, f.read())
+        send_reply(event.reply_token, f.read())
         f.close()
-
-
-def send(_token, _textmessage):
-    line_bot_api.reply_message(
-        _token,
-        TextSendMessage(text=_textmessage)
-    )
 
 
 if __name__ == "__main__":
